16c8f6a2ba37089d8126ba4f8d6237019dbda334
Corneliu Chitanu <info@chitanu.com>
2015-10-24 10:04:01 +0300

    Initial Commit

---
+++ PropertyReact/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PropertyReact
+//
+//  Created by Corneliu on 24/10/15.
+//  Copyright © 2015 Dolfn. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
